b97eb428a34cea4ab951cdf47476edf80369aafd
Quinn Cox <qrc.inpa@gmail.com>
2019-03-16 15:04:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,8 +20,8 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length > 0
-    katz_deli_number_1 = katz_deli[0]
-    puts "Currently serving #{katz_deli_number_1}."
+    # katz_deli_number_1 = katz_deli[0]
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift
   else
     puts "There is nobody waiting to be served!"
